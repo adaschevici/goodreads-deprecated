e80c75b6a67f36d115ed3459459075924629f85f
adaschevici <a.daschevici@gmail.com>
2019-01-26 21:37:08 +0000

    Add list loop with material components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,62 +1,21 @@
 import React, { Component } from 'react'
-import BookIcon from '@material-ui/icons/Book'
-import Button from '@material-ui/core/Button'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
-import TextField from '@material-ui/core/TextField'
+import BookIcon from '@material-ui/icons/Book'
 
 import './App.css'
 import { books } from '../../books.json'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.books = books.slice(0, 20)
-    this.state = {
-      books: books.slice(0, 20),
-      searchTerm: ''
-    }
-  }
-
-  renderButton = () => (
-    <Button
-      variant="contained"
-      size="medium"
-      color="primary"
-      onClick={this.onClick}
-    >
-      Search
-    </Button>
-  )
-
-  search = term => {
-    this.setState({
-      books: this.books.filter(book => book.title.includes(term))
-    })
-  }
-
-  onClick = () => this.search(this.state.searchTerm)
-
-  onChange = (event) => this.setState({ searchTerm: event.target.value })
-
   render() {
     console.log(books)
     const dense = true
     return (
       <div className="App">
-        <TextField
-          id="outlined-search"
-          label="Search field"
-          type="search"
-          margin="normal"
-          variant="outlined"
-          onChange={this.onChange}
-        />
-        {this.renderButton()}
         <List dense={dense}>
-          {this.state.books.map((book, index) => (<ListItem key={index}>
+          {books.map((book, index) => (<ListItem key={index}>
             <ListItemIcon>
               <BookIcon />
             </ListItemIcon>
