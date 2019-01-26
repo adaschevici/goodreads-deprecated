eb1df58f3d7c9dfd52a4947eeeeaecf7e7d6597c
adaschevici <a.daschevici@gmail.com>
2019-01-26 21:37:08 +0000

    Add list loop with material components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import BookIcon from '@material-ui/icons/Book'
+
 import './App.css'
-import books from '../../books.json'
+import { books } from '../../books.json'
 
 class App extends Component {
   render() {
+    console.log(books)
+    const dense = true
     return (
       <div className="App">
-        {console.log(books)}
+        <List dense={dense}>
+          {books.map((book, index) => (<ListItem key={index}>
+            <ListItemIcon>
+              <BookIcon />
+            </ListItemIcon>
+            <ListItemText primary={`${book.title} - ${book.author}`} />
+          </ListItem>))}
+        </List>
       </div>
     )
   }
