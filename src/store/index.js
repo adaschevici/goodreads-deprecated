7bfc3fb1544d2e1cbf6b1ca1d6076b0aff5e31cf
adaschevici <a.daschevici@gmail.com>
2019-02-12 21:38:33 +0000

    Add logger middleware

--- src/store/index.js
+++ src/store/index.js
@@ -7,13 +7,22 @@ const composeEnhancers = composeWithDevTools({
   trace: true
 })
 
-const customMiddleWare = store => next => action => {
-  console.log('Middleware triggered:', action)
-  console.log(store.getState())
-  next(action)
-}
+const createLogger = ({ getState }) =>
+  (next) =>
+    (action) => {
+      const console = window.console
+      const prevState = getState()
+      const returnValue = next(action)
+      const nextState = getState()
+      const actionType = String(action.type)
+      const message = `action ${actionType}`
+      console.log('%c prev state', 'color: #9E9E9E', prevState)
+      console.log('%c action', 'color: #03A9F4', message)
+      console.log('%c next state', 'color: #4CAF50', nextState)
+      return returnValue
+    }
 
-const middlewares = [thunk, customMiddleWare]
+const middlewares = [thunk, createLogger]
 
 export default function configureStore(initialState={}) {
   return createStore(
