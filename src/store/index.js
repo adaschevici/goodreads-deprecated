49d83e7795963b4fd3a51bc3812fd9ab6615c854
adaschevici <a.daschevici@gmail.com>
2019-02-12 19:58:55 +0000

    Add middleware reducer

--- src/store/index.js
+++ src/store/index.js
@@ -7,10 +7,18 @@ const composeEnhancers = composeWithDevTools({
   trace: true
 })
 
+const customMiddleWare = store => next => action => {
+  console.log('Middleware triggered:', action)
+  console.log(store.getState())
+  next(action)
+}
+
+const middlewares = [thunk, customMiddleWare]
+
 export default function configureStore(initialState={}) {
   return createStore(
     rootReducer,
     initialState,
-    composeEnhancers(applyMiddleware(thunk))
+    composeEnhancers(applyMiddleware(...middlewares))
   )
 }
